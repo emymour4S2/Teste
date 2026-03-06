f0f3fe47ba33cdabbd9cfb0c3651cdf730f81f80
emymour4S2 <emillymouralima405@gmail.com>
2026-03-06 09:32:10 -0300

    teste

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ("express")
+const app = express()
+
+app.get("/idade/:nome/:idade", (req, res)=>{
+    const {nome, idade} = req.params
+    res.status (200).json({
+        mensagem:`Nome: ${nome}, Idade: ${idade}`
+    })
+
+})
+
+app.listen(3020, ()=>{
+    console.log("Emilly o servidor está ligado")
+})
